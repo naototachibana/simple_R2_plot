07c19961afd566f71ce2de7a1e829534d0dba7b0
Naoto Tachibana <n173337@edu.tut.ac.jp>
2019-10-08 19:02:59 +0900

    Add files via upload

---
+++ simple_R2_plot.py
@@ -0,0 +1,83 @@
+# To add a new cell, type '#%%'
+# To add a new markdown cell, type '#%% [markdown]'
+
+#%%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+
+class simple_R2_plot :
+    def __init__(self, df_obs, df_pred,
+                 name = 'plot',
+                 xlabel = None, ylabel = None):
+        self.name = name
+        self.df_obs = df_obs
+        self.df_pred = df_pred
+        self.obs = df_obs.values
+        self.pred = df_pred.values
+        self.xlabel = df_obs.name
+        self.ylabel = df_pred.name
+        self.ymin = min (np.amin(self.pred), np.amin(self.obs))
+        self.ymax = max (np.amax(self.pred), np.amax(self.obs))
+        self.yrange = self.ymax - self.ymin
+        self.fig = None
+        self.RMSE = calc_RMSE()
+        self.R2 = self.calc_R2()
+        self.label_message = None
+        self.xlabel = self.df_obs.name
+        self.ylabel = self.df_pred.name
+        
+    def update_range(self,df_obs, df_pred):
+        if (self.ymax < np.amax(df_pred.values)):
+            self.ymax = np.amax(df_pred.values)
+            
+        if (self.ymin > np.amin(df_pred.values)):
+            self.ymin = np.amin(df_pred.values)
+        self.yrange = self.ymax - self.ymin
+    
+    def report_max (self):
+        return self.ymax
+    def report_min (self):
+        return self.ymin
+    
+    def calc_R2 (self):
+        return r2_score(self.df_obs, self.df_pred)
+    def calc_RMSE (self):
+        return np.sqrt(mean_squared_error(self.df_obs, self.df_pred))
+
+    def plot (self, energy_max, energy_min, xlabel, ylabel):
+        self.fig = plt.figure(figsize=(8, 8))
+        self.yrange = energy_max - energy_min
+        xmin = energy_min - self.yrange * 0.01
+        xmax = energy_max + self.yrange * 0.01
+        ymin = energy_min- self.yrange * 0.01
+        ymax = energy_max + self.yrange * 0.01
+        plt.plot([xmin, xmax], 
+                 [ymin, ymax])
+
+        plt.xlim(xmin,xmax)
+
+        plt.ylim(ymin,ymax)
+        self.RMSE = self.calc_RMSE()
+        self.R2 = self.calc_R2()
+        self.label_message = ("RMSE : " + str (round(self.RMSE, 5))
+                  +"\nR2      : "+ str(round(self.R2, 5)))
+        
+        plt.scatter(self.obs, self.pred, label=self.label_message)
+        plt.legend(fontsize=12, loc='upper left')
+
+        plt.xlabel(xlabel, fontsize=24)
+        plt.ylabel(ylabel, fontsize=24)
+        plt.title('Observed-Predicted Plot\n('+self.name+')', fontsize=24)
+        output_filename = 'observed-predicted_plot_'+self.name+'.png'
+        plt.tick_params(labelsize=16)
+        plt.grid()
+        self.fig.savefig(output_filename,bbox_inches="tight")  #図をFileに保存する。
+        display_png(Image(output_filename))
+
+
+#%%
+if __name__ == '__main__':
+
+
